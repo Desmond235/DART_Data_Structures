c7a7c593beb83fdecb7394080dd3208e45aca4cf
Desmond <adabedesmond@gmail.com>
2025-04-18 15:46:22 -0700

    modified

--- lib/list.dart
+++ lib/list.dart
@@ -1,8 +1,30 @@
+void main(List<String> args) {
+  List<String> fruits = ['apple', 'mango', 'orange', 'watermelon'];
 
-void main(List<String>args){
-  List<String>fruits = ['apple', 'mango', 'orange','watermelon'];
-
-  fruits = <String>[ for ( var fruit in fruits) fruit.toUpperCase()];
+  fruits = <String>[for (var fruit in fruits) fruit.toUpperCase()];
 
   print(fruits.join("\n"));
+
+  var address = <dynamic>[
+    Address(street: '123 fake street', state: 'Abudja', zipCode: '123')
+  ];
+  address = [for(var add in address) [add.state, add.street, add.zipCode].join("\n")];
+  print(address.join(""));
+
+  // for (var add in address){
+  //   print(add.state);
+  // }
+
+}
+
+class Address {
+  final String street;
+  final String state;
+  final String zipCode;
+
+  Address({
+    required this.street,
+    required this.state,
+    required this.zipCode,
+  });
 }
