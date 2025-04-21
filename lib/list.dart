58076fc3f84b4cf7c6c6af9d9a57cfdb99e94f26
Desmond <adabedesmond@gmail.com>
2025-04-20 22:59:33 -0700

    modified list

--- lib/list.dart
+++ lib/list.dart
@@ -8,7 +8,8 @@ void main(List<String> args) {
   var address = <dynamic>[
     Address(street: '123 fake street', state: 'Abudja', zipCode: '123')
   ];
-  address = [for(var add in address) [add.state, add.street, add.zipCode].join("\n")];
+  address = [for(var add in address)
+   [add.state[0].toUpperCase()+ add.state.substring(1), add.street, add.zipCode].join("\n")];
   print(address.join(""));
 
   // for (var add in address){
