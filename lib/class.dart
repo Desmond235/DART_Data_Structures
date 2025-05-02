67d8259416df47fcfbbb8983323cb91fd7594eee
Desmond <adabedesmond@gmail.com>
2025-05-01 20:02:53 -0700

    modified

--- lib/class.dart
+++ lib/class.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: unused_local_variable
+
 extension ComprableOperators<T extends Comparable<T>> on T{
   bool operator <(T other) => compareTo(other) < 0;
   bool operator >(T other) => compareTo(other) > 0;
@@ -43,13 +45,29 @@ class Book implements Comparable<Book>{
      }
 }
 
+class Rectangle{
+   double _width;
+   double _height;
+
+  Rectangle(double width, double height) : _width = width , _height = height;
+
+  double get width => _width;
+  double get height => _height;
+
+
+  set width(double width) => _width = width;
+  set height(double height) => _height = height;
+}
 void main(List<String> args) {
   var book1 = Book("Hobbit", "J.R.R Tolkein", 310);
   var book2 =
       Book("Harry Porter and the Philosopher's Stone", "J.K. Rowling", 23);
   var book3 = Book("The Lion, the Witch and the Wardrobe", 'C.S Lewis', 192);
 
-  print(book1);
-  print(book1 == book2);
-  print(book2 > book3);
+  var rectangle = Rectangle(6, 7);
+  
+  rectangle.width = 5;
+  rectangle.height = 8;
+  print(rectangle.width);
+  print(rectangle.height);
 }
